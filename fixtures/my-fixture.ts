a3086a61ec9ce7414259c60e536755aae1e369e2
Harsh <harsh.singal31@gmail.com>
2026-07-01 13:49:27 +1000

    add one todo Item fixture

--- fixtures/my-fixture.ts
+++ fixtures/my-fixture.ts
@@ -3,11 +3,17 @@ import HomePage from "../models/pages/home.page";
 
 type MyFixtures = {
   homePage: HomePage;
+  addOneToDo: HomePage;
 };
 export const test = base.extend<MyFixtures>({
   homePage: async ({ page }, use) => {
     const homePage = new HomePage(page);
     await homePage.goto();
+    await use(homePage);
+  },
+
+  addOneToDo: async ({ homePage }, use) => {
+    homePage.addTodoItem("Buy Groceries");
     await use(homePage);
   },
 });
